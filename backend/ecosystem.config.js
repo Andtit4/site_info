28a617f5a22499c4dd365d38d8fa4b205e4421dd
Andtit4 <lirsitogo2021@gmail.com>
2025-04-26 18:07:34 +0000

    update ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
             DATABASE_USERNAME: 'u527740812_site_info_dbus',
             DATABASE_PASSWORD: 'Motdep@sse/2022',
             DATABASE_NAME: 'u527740812_site_info_db',
-            API_PORT: 2404,
+            API_PORT: 5001,
             API_PREFIX: 'api',
             JWT_SECRET: 'u3jkh54sdp98sd38suh38fyhs38fhs9fd',
             ADMIN_SETUP_KEY: 'GhsZkdIzm8JSt7F9Q3EpL6vNwRx2Ay5U'
--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:3000/api'
+const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:5001/api'
 
 // Configuration d'axios 
 axios.interceptors.request.use(
